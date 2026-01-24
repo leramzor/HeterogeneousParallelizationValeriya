0999d386ae4731a6958aa396b35ce41006a2eaae
leramzor <67414497+leramzor@users.noreply.github.com>
2026-01-25 01:47:58 +0500

    Add files via upload

---
+++ Practice10/Practice10Task4MPI.cpp
@@ -0,0 +1,33 @@
+﻿#include <mpi.h>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv); // Инициализация распределенной среды
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Кто я?
+    MPI_Comm_size(MPI_COMM_WORLD, &size); // Сколько нас всего?
+
+    long N = 10000000; // Общий размер задачи
+    double local_sum = 0, global_sum = 0;
+
+    double start = MPI_Wtime(); // [Лекция 10, стр. 12] Замер времени в MPI
+
+    // Распределяем работу (каждый считает свою часть)
+    for (long i = 0; i < N / size; i++) {
+        local_sum += 1.0;
+    }
+
+    // [Лекция 9, стр. 13] Коллективная операция — узкое место при плохой сети
+    MPI_Reduce(&local_sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    double end = MPI_Wtime();
+
+    if (rank == 0) {
+        std::cout << ">>> Practice10Task4MPI " << size;
+        std::cout << ">>> [MPI] Procs: " << size
+            << " | Time: " << (end - start) << "s" << std::endl;
+    }
+
+    MPI_Finalize(); // Завершение работы
+    return 0;
+}
